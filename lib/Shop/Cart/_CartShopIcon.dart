24efd3b1a11931c3ae4b7bcc763b20fe018db3aa
Home <e.jawadfazza@gmail.com>
2023-12-03 21:47:13 +0300

    ..

--- lib/Shop/Cart/_CartShopIcon.dart
+++ lib/Shop/Cart/_CartShopIcon.dart
@@ -15,7 +15,7 @@ class CartShopIcon extends StatelessWidget{
       },
       icon: Stack(
         children: [
-          Icon(Icons.shopping_cart_rounded),
+          const Icon(Icons.shopping_cart_rounded),
           Positioned(
             top: 0,
             right: 0,
@@ -23,14 +23,14 @@ class CartShopIcon extends StatelessWidget{
               valueListenable: ShoppingCart.cartItemCount,
               builder: (context, count, child) {
                 return Container(
-                  padding: EdgeInsets.all(4),
-                  decoration: BoxDecoration(
+                  padding: const EdgeInsets.all(4),
+                  decoration: const BoxDecoration(
                     shape: BoxShape.circle,
                     color: Colors.red, // You can change the color
                   ),
                   child: Text(
                     '$count',
-                    style: TextStyle(
+                    style: const TextStyle(
                       color: Colors.white,
                       fontWeight: FontWeight.bold,
                       fontSize: 12,
